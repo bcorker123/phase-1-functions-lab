16ef09723724ca6a4bfb54ed701b8db91d671767
brian corker <bcorker123@gmail.com>
2022-04-25 11:53:44 -0400

    commit tree 2f0dd5e270f2f385d5de62beb051a8212fb3538c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    return Math.abs(42 - pickUpLocation);
+};
+
+function distanceFromHqInFeet(pickUpLocation) {
+    return 264 * distanceFromHqInBlocks(pickUpLocation);
+};
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start) * 264);
+};
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    } else if (distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    }
+};
